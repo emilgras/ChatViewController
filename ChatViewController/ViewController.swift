092ef225e73efb058b8f2a9656e767b2d2ca65cf
emilgras <emil.gras@gmail.com>
2016-09-11 13:01:44 +0200

    Initial Commit

---
+++ ChatViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatViewController
+//
+//  Created by Emil Gräs on 11/09/2016.
+//  Copyright © 2016 Emil Gräs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
